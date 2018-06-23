d6d0ca4fd7010d8efb36a2d04ca675e31d9f5d76
Ryder Smith <majorpunk@gmail.com>
2018-06-23 11:58:41 -0500

    Started on a model for a led stand. Started working of the layout of the page.

---
+++ web_controller/portal/src/components/ViewPort.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card raised class="view-port" light>
+    <v-card-media class="view-port__media" src="/static/20180622_191404.jpg" contain></v-card-media>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ViewPort',
+}
+</script>
+
+<style scoped>
+.view-port {
+}
+.view-port__media {
+  height: 0;
+  padding-top: 56.25%;
+}
+</style>
---
+++ web_controller/portal/src/pages/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-layout class="layout" row>
+      <v-flex xs12 sm8 offset-sm2>
+        <view-port/>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import ViewPort from '@/components/ViewPort'
+
+export default {
+  name: 'Main',
+  components: {
+    ViewPort,
+  },
+}
+</script>
+
+<style scoped>
+</style>
